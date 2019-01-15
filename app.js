928df921475399044da191058e64d3e46d21b685
ShyBo <beau.shirdavani@bellevuecollege.edu>
2019-01-15 23:55:24 +0000

    mongo DB added with mongoose

--- app.js
+++ app.js
@@ -2,34 +2,35 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-//------------------------------------------------------------------ DATA
-    // temp data before DB implementation
-    var campgrounds = [
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"},
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"},
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"},
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"},
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"},
-            {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-            {name: "Granite Hill", image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"},
-            {name: "Fart Peak", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f7c57ea1e5b4b0_340.jpg"}
-        ];
 
+//------------------------------------------------------------------ DATA
+// schema setup for mongo db
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+// embed schema into a model
+var Campground = mongoose.model("Campground", campgroundSchema);
+// add the existing campgrounds
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"
+//     }, function(err, campground){
+//         if(err){
+//             console.log("error: ", err);
+//         }else{
+//             console.log("newly created campground");
+//             console.log(campground);
+//         }
+//     });
 
 
 //------------------------------------------------------------------ ROUTES 
@@ -41,7 +42,17 @@ app.get("/", function(req, res){
 
 // camp grounds route
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // get all campgrounds from mongo db
+    Campground.find({}, function(error, allCampgrounds){
+        if(error){
+            console.log("error:");
+            console.log(error);
+        }else{
+            // render the file
+            console.log("found camp grounds");
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
 // add campground route
@@ -50,9 +61,17 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+    // create a new campbround, and save to mongo database
+    Campground.create(newCampground, function(error, newlyCreated){
+        if(error){
+            console.log("error:", error)
+        }else{
+            // redirect back to /campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
     // redirect back to /campgrounds page
-    res.redirect("/campgrounds");
+    // res.redirect("/campgrounds");
 });
 // add form for new campground route
 app.get("/campgrounds/new", function(req, res){
