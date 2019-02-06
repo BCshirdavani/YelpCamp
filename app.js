bce443bfbcfe186794cdf56d6847f528deb1ddb0
ShyBo <beau.shirdavani@bellevuecollege.edu>
2019-02-06 23:03:59 +0000

    installed auth packages, defined User model

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var Campground = require("./models/campground.js");
 var Comment = require("./models/comment.js");
 // var User = require("./models/user.js");
 var seedDB = require("./seeds.js");
+var passport = require('passport');
+var LocalStrategy = require("passport-local");
 
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", { useNewUrlParser: true });
 seedDB();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));         // include css style directory
 
 
 
